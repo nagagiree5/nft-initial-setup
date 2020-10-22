ce64e358e0b36f0a5eb8cc43649c45c7b0b43903
nagagirigollapalli@gmail.com <nagagirigollapalli@gmail.com>
2020-10-22 11:45:15 +0530

    Report user service initial setup...

---
+++ src/main/java/io/springboot2/x/NftUserReportApplication.java
@@ -0,0 +1,13 @@
+package io.springboot2.x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftUserReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftUserReportApplication.class, args);
+	}
+
+}
