dbaf8637a0c8faf76782ff445732cf2c84bea586
nagagirigollapalli5@gmail.com <nagagirigollapalli5@gmail.com>
2020-10-22 08:47:46 +0530

    Initial commit....

---
+++ nft-initial-setup/src/main/java/io/springboot2/x/nftinitialsetup/NftInitialSetupApplication.java
@@ -0,0 +1,13 @@
+package io.springboot2.x.nftinitialsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftInitialSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftInitialSetupApplication.class, args);
+	}
+
+}
---
+++ nft-initial-setup/src/test/java/io/springboot2/x/nftinitialsetup/NftInitialSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot2.x.nftinitialsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftInitialSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
